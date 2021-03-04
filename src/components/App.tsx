6130c423c1a5db15e7869c48af1e0a5465bcdaf2
MattLiu-mygit <mliu78@jh.edu>
2021-03-04 14:12:55 -0500

    Migrated project to redux-typescript template

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,22 @@
+import * as React from 'react';
+import { useEffect } from 'react';
 import Header from './header/Header';
 import Content from './body/Content';
+import { Counter } from '../features/redux_sample/Counter';
 
 function App() {
+  useEffect(() => {
+    // Get user
+  });
   return (
-    <div 
-      style={{display:'flex',
-      flexFlow: 'column', 
-      height:window.innerHeight
-      }}>
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'column',
+        height: window.innerHeight,
+      }}
+    >
+      <Counter />
       <Header />
       <Content />
     </div>
