88b363fb260f81b27266e9732c2e628184cfbdd0
MattLiu-mygit <mliu78@jh.edu>
2021-03-04 15:23:46 -0500

    More redux integrations

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import { useEffect } from 'react';
 import Header from './header/Header';
 import Content from './body/Content';
-import { Counter } from '../features/redux_sample/Counter';
+import { Counter } from '../redux_sample/Counter';
+import { useDispatch } from 'react-redux';
+import { login } from './slices/userSlice';
+import { testUser } from './testObjs';
 
 function App() {
+  const dispatch = useDispatch();
   useEffect(() => {
     // Get user
-  });
+    dispatch(login(testUser));
+  }, []);
   return (
     <div
       style={{
@@ -16,7 +21,7 @@ function App() {
         height: window.innerHeight,
       }}
     >
-      <Counter />
+      {/* <Counter /> */}
       <Header />
       <Content />
     </div>
