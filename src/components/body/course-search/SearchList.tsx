dfdd557dc428ced9fafb73340b3909be289822a2
MattLiu-mygit <mliu78@jh.edu>
2021-03-26 13:55:06 -0400

    Pagination added.

--- src/components/body/course-search/SearchList.tsx
+++ src/components/body/course-search/SearchList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectRetrievedCourses } from '../../slices/searchSlice';
 import CourseCard from './CourseCard';
+import ReactPaginate from 'react-paginate';
 
 const SearchList = () => {
   const courses = useSelector(selectRetrievedCourses);
@@ -9,10 +10,28 @@ const SearchList = () => {
   // Generates a list of retrieved course matching the search queries
   const courseList = () =>
     courses.map((course) => <CourseCard course={course} />);
+  const handlePageClick = () => {};
   return (
     <div className="flex-1 m-3 p-2 bg-gray-300">
       <p>Search Results </p>
       <div className="flex flex-col overflow-vertical">{courseList()}</div>
+      {/* A Pagination component we'll use! Prop list and docs here: https://github.com/AdeleD/react-paginate. '
+      Use it to add new classnames when styling and add new props for logic */}
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        previousClassName={'m-2'}
+        nextClassName={'m-2'}
+        breakLabel={'...'}
+        breakClassName={''}
+        pageCount={3}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'flex'}
+        activeClassName={'bg-gray-500'}
+        pageClassName={'m-2'}
+      />
     </div>
   );
 };
