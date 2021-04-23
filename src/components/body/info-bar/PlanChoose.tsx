8dcc887a041dd05c6624ebbca60d910e8d2e787e
MattLiu-mygit <mliu78@jh.edu>
2021-04-23 13:12:26 -0400

    Added extra logging

--- src/components/body/info-bar/PlanChoose.tsx
+++ src/components/body/info-bar/PlanChoose.tsx
@@ -29,100 +29,111 @@ const PlanChoose: React.FC<PlanChooseProps> = (props) => {
 
   // Gets all users's plans and updates state everytime a new user is chosen.
   useEffect(() => {
-    if (user._id !== "") {
-      axios.get(api + "/plansByUser/" + user._id).then((retrieved) => {
-        const retrievedPlans: Plan[] = retrieved.data.data;
-        if (retrievedPlans.length > 0) {
-          // sort plans by ids if there is more than one plan
-          retrievedPlans.sort((plan1: Plan, plan2: Plan) =>
-            plan1._id.localeCompare(plan2._id)
-          );
-        }
-
-        if (currentPlan._id !== "noPlan") {
-          // Swap first plan in the list with the current plan.
-          retrievedPlans.forEach((plan: Plan, index) => {
-            if (plan._id === currentPlan._id) {
-              const temp = retrievedPlans[0];
-              retrievedPlans[0] = currentPlan;
-              retrievedPlans[index] = temp;
-            }
-          });
-        }
-
-        if (retrievedPlans.length > 0 && currentPlan._id === "noPlan") {
-          // Initial load, there is no current plan, so we set the current to be the first plan in the array.
-          dispatch(updatePlanList(retrievedPlans));
-          dispatch(updateSelectedPlan(retrievedPlans[0]));
-        } else if (
-          retrievedPlans.length === 0 &&
-          user._id !== "noUser" &&
-          user._id !== "guestUser"
-        ) {
-          // If no plans, automatically generate a new plan
-          // TODO: Modularize creating courses into its own common function
-          // GenerateNewPlan(user, retrievedPlans);
-          // .then(() => {
-          //         if (
-          //           index ===
-          //           testMajorCSNew.generalDistributions.length - 1
-          //         ) {
-          //           dispatch(updateSelectedPlan(newRetrievedPlan));
-          //           dispatch(updatePlanList(retrievedPlans));
-          //           props.setNewPlan(props.newPlan + 1);
-          //         }
-          //       });
-          //   }
-          console.log("new plan 1");
-          const planBody = {
-            name: "Unnamed Plan",
-            user_id: user._id,
-            majors: [testMajorCSNew],
-          };
-          axios.post(api + "/plans", planBody).then((data: any) => {
-            let newRetrievedPlan = { ...data.data.data };
-            testMajorCSNew.generalDistributions.forEach(
-              (distr: any, index: number) => {
-                axios
-                  .post(api + "/distributions", {
-                    name: distr.name,
-                    required: distr.required,
-                    user_id: user._id,
-                    plan_id: newRetrievedPlan._id,
-                  })
-                  .then((newDistr: any) => {
-                    newRetrievedPlan = {
-                      ...newRetrievedPlan,
-                      distribution_ids: [
-                        ...newRetrievedPlan.distribution_ids,
-                        newDistr.data.data._id,
-                      ],
-                    };
-                  })
-                  .then(() => {
-                    if (
-                      index ===
-                      testMajorCSNew.generalDistributions.length - 1
-                    ) {
-                      dispatch(updateSelectedPlan(newRetrievedPlan));
-                      props.setNewPlan(props.newPlan + 1);
-                      if (user._id === "guestUser") {
-                        const planIdArray = [newRetrievedPlan._id];
-                        dispatch(updateGuestPlanIds(planIdArray));
-                        dispatch(
-                          updatePlanList([newRetrievedPlan, ...planList])
-                        );
-                      }
-                    }
-                  });
-              }
+    if (user._id !== "noUser") {
+      axios
+        .get(api + "/plansByUser/" + user._id)
+        .then((retrieved) => {
+          const retrievedPlans: Plan[] = retrieved.data.data;
+          if (retrievedPlans.length > 0) {
+            // sort plans by ids if there is more than one plan
+            retrievedPlans.sort((plan1: Plan, plan2: Plan) =>
+              plan1._id.localeCompare(plan2._id)
             );
-          });
-        } else {
-          // If there is already a current plan, simply update the plan list.
-          dispatch(updatePlanList(retrievedPlans));
-        }
-      });
+          }
+
+          if (currentPlan._id !== "noPlan") {
+            // Swap first plan in the list with the current plan.
+            retrievedPlans.forEach((plan: Plan, index) => {
+              if (plan._id === currentPlan._id) {
+                const temp = retrievedPlans[0];
+                retrievedPlans[0] = currentPlan;
+                retrievedPlans[index] = temp;
+              }
+            });
+          }
+
+          if (retrievedPlans.length > 0 && currentPlan._id === "noPlan") {
+            // Initial load, there is no current plan, so we set the current to be the first plan in the array.
+            dispatch(updatePlanList(retrievedPlans));
+            dispatch(updateSelectedPlan(retrievedPlans[0]));
+          } else if (
+            retrievedPlans.length === 0 &&
+            user._id !== "noUser" &&
+            user._id !== "guestUser"
+          ) {
+            // If no plans, automatically generate a new plan
+            // TODO: Modularize creating courses into its own common function
+            // GenerateNewPlan(user, retrievedPlans);
+            // .then(() => {
+            //         if (
+            //           index ===
+            //           testMajorCSNew.generalDistributions.length - 1
+            //         ) {
+            //           dispatch(updateSelectedPlan(newRetrievedPlan));
+            //           dispatch(updatePlanList(retrievedPlans));
+            //           props.setNewPlan(props.newPlan + 1);
+            //         }
+            //       });
+            //   }
+            console.log("new plan 1");
+            const planBody = {
+              name: "Unnamed Plan",
+              user_id: user._id,
+              majors: [testMajorCSNew],
+            };
+            axios.post(api + "/plans", planBody).then((data: any) => {
+              let newRetrievedPlan = { ...data.data.data };
+              testMajorCSNew.generalDistributions.forEach(
+                (distr: any, index: number) => {
+                  axios
+                    .post(api + "/distributions", {
+                      name: distr.name,
+                      required: distr.required,
+                      user_id: user._id,
+                      plan_id: newRetrievedPlan._id,
+                    })
+                    .then((newDistr: any) => {
+                      newRetrievedPlan = {
+                        ...newRetrievedPlan,
+                        distribution_ids: [
+                          ...newRetrievedPlan.distribution_ids,
+                          newDistr.data.data._id,
+                        ],
+                      };
+                    })
+                    .then(() => {
+                      if (
+                        index ===
+                        testMajorCSNew.generalDistributions.length - 1
+                      ) {
+                        dispatch(updateSelectedPlan(newRetrievedPlan));
+                        props.setNewPlan(props.newPlan + 1);
+                        if (user._id === "guestUser") {
+                          const planIdArray = [newRetrievedPlan._id];
+                          dispatch(updateGuestPlanIds(planIdArray));
+                          dispatch(
+                            updatePlanList([newRetrievedPlan, ...planList])
+                          );
+                        }
+                      }
+                    });
+                }
+              );
+            });
+          } else {
+            // If there is already a current plan, simply update the plan list.
+            dispatch(updatePlanList(retrievedPlans));
+          }
+        })
+        .catch((err) => {
+          if (user._id === "guestUser") {
+            console.log(
+              "In guest user! This is expected as there are no users with this id."
+            );
+          } else {
+            console.log(err);
+          }
+        });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, props.newPlan, planList.length]);
