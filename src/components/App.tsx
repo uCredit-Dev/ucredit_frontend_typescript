1aece26d209240bc7453a37fdc2dfd6bf7fa1096
MattLiu-mygit <mliu78@jh.edu>
2021-05-25 04:39:14 -0400

    Trying to make global cookies

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,39 @@
 import * as React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import DashboardEntry from "./DashboardEntry";
 import bird from "./birdTempGif.gif";
-//import { Counter } from '../redux_sample/Counter';
+import { withCookies, useCookies } from "react-cookie";
 
 /* 
   Root app component.
 */
-function App() {
+function App(props: any) {
+  // Component state setup
+  const [cookies, setCookies] = useState(props.cookies);
+  const [authCookies, setAuthCookie] = useCookies(["connect.sid"]);
+  const [cookieUpdate, setCookieUpdate] = useState<boolean>(true);
+
+  let history = useHistory();
+  let location = useLocation();
+
+  // Creates a cookie based on url.
+  const createCookie = (token: string) => {
+    if (!token.includes("dashboard")) {
+      setAuthCookie("connect.sid", token);
+      setCookies(props.cookies);
+      setCookieUpdate(!cookieUpdate);
+      history.push("/");
+    }
+  };
+
   const [welcomeScreen, setWelcomeScreen] = useState<boolean>(true);
   useEffect(() => {
     setTimeout(() => setWelcomeScreen(false), 1500);
   }, []);
+
   return (
     <>
       {welcomeScreen ? (
@@ -31,11 +50,11 @@ function App() {
           <Dashboard />
         </Route>
         <Route path="/">
-          <DashboardEntry />
+          <DashboardEntry createCookie={createCookie} />
         </Route>
       </Switch>
     </>
   );
 }
 
-export default App;
+export default withCookies(App);
