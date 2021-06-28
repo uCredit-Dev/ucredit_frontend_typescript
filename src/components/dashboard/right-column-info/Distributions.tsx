22496291cd5eadd8635a0c9415bdcd65bb0ff1b6
Alan Zhang <azhang42@jhu.edu>
2021-06-28 14:07:26 -0400

    Updated Distributions Design

--- src/components/dashboard/right-column-info/Distributions.tsx
+++ src/components/dashboard/right-column-info/Distributions.tsx
@@ -14,6 +14,8 @@ import {
   updateFulfilled,
   getRequirements,
 } from "./distributionFunctions";
+import { ReactComponent as Check } from "../../../resources/svg/CheckMark.svg"
+import { ReactComponent as X } from "../../../resources/svg/Close.svg"
 
 const Distributions = () => {
   const currentPlan = useSelector(selectPlan);
@@ -26,7 +28,7 @@ const Distributions = () => {
   >([]);
   const [distributionOpen, setDistributionOpen] = useState<boolean>(true);
   const [displayGeneral, setDisplayGeneral] = useState<boolean>(true); // Sets all distributions for distribution bars.
-
+  const [showDistributions, setShowDistributions] = useState<boolean[]>(new Array(distributions.length));
   // Gets distribution everytime a plan changes.
   useEffect(() => {
     const distr = getDistributions();
@@ -54,6 +56,12 @@ const Distributions = () => {
     ReactTooltip.rebuild();
   }, [displayGeneral, distributions]);
 
+  const changeDistributionVisibility = (i : number) => {
+    let showDistributionsCopy = showDistributions.slice();
+    showDistributionsCopy[i] = !showDistributions[i];
+    setShowDistributions(showDistributionsCopy);
+  }
+
   return (
     <div className="flex-none ml-4 mr-4 p-6 w-coursebars h-auto bg-white rounded shadow">
       <div className="flex flex-row mb-3 w-full">
@@ -71,70 +79,45 @@ const Distributions = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-row justify-evenly my-4">
-        <button
-          onClick={() => setDisplayGeneral(true)}
-          className={clsx(
-            "focus:outline-none transform hover:scale-110 transition duration-200 ease-in",
-            {
-              underline: displayGeneral,
-            }
-          )}
-        >
-          General
-        </button>
-        <button
-          onClick={() => setDisplayGeneral(false)}
-          className={clsx(
-            "focus:outline-none transform hover:scale-110 transition duration-200 ease-in",
-            {
-              underline: !displayGeneral,
-            }
-          )}
-        >
-          Fine
-        </button>
-      </div>
-      {distributionOpen
-        ? distributions.map((pair) => {
-            if (displayGeneral) {
-              const dis = pair[1][0];
-              return (
-                <div key={dis.name}>
-                  <CourseBar
-                    maxCredits={dis.required_credits}
-                    plannedCredits={dis.fulfilled_credits}
-                    currentCredits={dis.required_credits}
-                    section={dis.name}
-                    general={true}
-                  />
-                </div>
-              );
-            } else {
-              let general = "";
-              return pair[1].map((dis, index) => {
-                if (index === 0) {
-                  general = dis.name;
-                  return <></>;
-                }
+      {distributions.map((pair, i) => {
+        return (
+          <div>
+            {pair[1].map((dis, index) => {
+              if (index === 0) {
                 return (
-                  <div key={dis.name}>
-                    <div className="font-bold">
-                      {index === 1 ? general : null}
-                    </div>
+                  <div key={dis.name} className={clsx({"hidden" : !distributionOpen})}>
                     <CourseBar
                       maxCredits={dis.required_credits}
                       plannedCredits={dis.fulfilled_credits}
                       currentCredits={dis.required_credits}
                       section={dis.name}
-                      general={false}
+                      general={true}
                     />
                   </div>
                 );
-              });
-            }
-          })
-        : null}
+              } else {
+                return showDistributions[i] === true ? (
+                  <div key={dis.name} className={clsx("flex justify-between", {"hidden" : !distributionOpen})}>
+                    <div className="flex mb-1 whitespace-nowrap overflow-hidden overflow-ellipsis pr-2 w-full">
+                      <div>
+                        {dis.fulfilled_credits >= dis.required_credits ? <Check fill="green"/> : <X stroke="red"/>}
+                      </div>
+                      <p className="whitespace-nowrap overflow-hidden overflow-ellipsis pr-2">{dis.name}</p>
+                    </div>
+                    <p className="font-bold">{dis.fulfilled_credits}/{dis.required_credits}</p>
+                  </div> 
+                ) : null
+              }
+            })}
+            <button
+              onClick={() => {changeDistributionVisibility(i)}}
+              className={clsx("focus:outline-none transform hover:scale-110 transition duration-200 ease-in underline", {"hidden" : !distributionOpen})}
+            >
+              {showDistributions[i] === true ? "Show Less" : "Show More"}
+            </button>
+          </div>
+        )
+      })}
     </div>
   );
 };
