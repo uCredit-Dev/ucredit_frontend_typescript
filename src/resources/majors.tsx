804431b7411212baddcfc1c5e96499cb0b7aa4df
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2021-08-01 16:54:32 -0400

    added CS (OLD) prerequisites

--- src/resources/majors.tsx
+++ src/resources/majors.tsx
@@ -1085,15 +1085,6 @@ const bsAMS: Major = {
   department: "EN Applied Mathematics & Statistics",
   total_degree_credit: 120,
   wi_credit: 6,
-  distributions: [],
-};
-
-// TODO: Update requirements!
-const bsCS_Old: Major = {
-  degree_name: "B.S. Computer Science(OLD)",
-  department: "EN Computer Science",
-  total_degree_credit: 120,
-  wi_credit: 6,
   distributions: [
     {
       name: "Computer Science",
@@ -1212,9 +1203,133 @@ const bsCS_Old: Major = {
   ],
 };
 
+// https://www.cs.jhu.edu/undergraduate-studies/academics/ugrad-advising-manual/
+const bsCS_Old: Major = {
+  degree_name: "B.S. Computer Science (OLD - Pre-2021)",
+  department: "EN Computer Science",
+  total_degree_credit: 126,
+  wi_credit: 6,
+  distributions: [
+    {
+      name: "Computer Science",
+      required_credits: 45,
+      min_credits_per_course: 1,
+      description:
+        "For more information please visit the <a href='https://www.cs.jhu.edu/2021undergraduate-advising-manual/'>" +
+        "major degree requirement</a> section on the department website.",
+      criteria: "EN Computer Science[D]^OR^CSCI-OTHER[T]",
+      fine_requirements: [
+        {
+          description:
+            "Computer Ethics(601.104). Practical Ethics for Future Leaders (660.400/406) " +
+            "may be used as a substitute for the computer ethics requirement for the BS program, " +
+            "but does not count towards the CS total credits at all.",
+          required_credits: 1,
+          criteria: "EN.600.104[C]^OR^EN.601.104[C]",
+        },
+        {
+          description:
+            "Required Courses: \n\t500.112/113/114 Gateway Computing or AP Comp Sci A or " +
+            "equivalent\n\t601.220 Intermediate Programming\n\t601.226 Data Structures\n\t601.229 " +
+            "Computer System Fundamentals\n\t600.231/271 Automata and Computation Theory\n\t601.433 Algorithms",
+          required_credits: 20,
+          criteria:
+            "EN.500.112[C]^OR^EN.500.113[C]^OR^EN.500.114[C]^OR^EN.601.220[C]^OR^EN.601.226[C]" +
+            "^OR^EN.601.229[C]^OR^EN.600.231[C]^OR^EN.600.271[C]^OR^EN.601.443[C]",
+        },
+        {
+          description:
+            "13 upper level CS credits in addition to the required Algorithms course",
+          required_credits: 13,
+          criteria: "EN Computer Science[D]^AND^Upper Level Undergraduate[L]",
+        },
+        {
+          description:
+            "At least one upper level course in two of these four different classification " +
+            "areas: Applications(CSCI-APPL), Systems(CSCI-SYST), Software(CSCI-SOFT) and Reasoning(CSCI-RSNG)",
+          required_credits: 8,
+          exclusive: true,
+          criteria:
+            "CSCI-APPL[T]^OR^CSCI-SYST[T]^OR^CSCI-SOFT[T]^OR^CSCI-RSNG[T]",
+        },
+        {
+          description:
+            "One Team(CSCI-TEAM) designated course. This Team course may overlap other course " +
+            "requirements, for example to count as both Team and Software.",
+          required_credits: 3,
+          criteria: "CSCI-TEAM[T]",
+        },
+      ],
+    },
+    {
+      name: "Math",
+      required_credits: 24,
+      min_credits_per_course: 3,
+      description:
+        "All courses in this category must be from one of the two math departments on " +
+        "campus: Mathematics or Applied Math and Statistics. However, 553.171 Discrete Mathematics " +
+        "may not count towards these math requirements. Other than Calculus I and II, all the " +
+        "remaining courses must be 200-level or above. The BS math courses must include coverage " +
+        "of both probability and statistics, which can be satisfied in many ways, including " +
+        "taking any of the 553.3xx combined Probability & Statistics courses.",
+      criteria: "AS Mathematics[D]^OR^EN Applied Math and Statistics[D]",
+      exception: "EN.553.171[C]",
+      fine_requirements: [
+        {
+          description:
+            "Required Courses:\n\t110.108 Calculus I or AP equivalent\n\t110.109 Calculus II or AP equivalent\n\t" +
+            "550.171/553.171 Discrete Mathematics",
+          required_credits: 12,
+          criteria: "AS.110.108[C]^OR^AS.110.109[C]^OR^EN.550.171[C]^OR^EN.553.171[C]",
+        },
+        {
+          description: "Probability and Statistics",
+          required_credits: 4,
+          criteria:
+            "EN Applied Mathematics & Statistics[D]^AND^(Probability & Statistics[N]^OR^Probability and Statistics[N])^AND^Upper Level Undergraduate[L]",
+        },
+      ],
+    },
+    {
+      name: "Science",
+      required_credits: 16,
+      min_credits_per_course: 1,
+      description:
+        "At least two semesters of physics or two semesters of chemistry, with the associated laboratories, must be included.",
+      criteria: "N[A]",
+    },
+    {
+      name: "Liberal Arts",
+      required_credits: 18,
+      min_credits_per_course: 3,
+      double_count: true,
+      description:
+        "These courses must have either an ‘H’ or ‘S’ area designator on them, but can be " +
+        "from any department. At most 2 of these courses may be taken S/U (if not counted towards " +
+        "the writing requirement). Foreign language courses can be counted as well, even if " +
+        "they don’t carry an ‘H’ or ‘S’ designator.",
+      criteria:
+        "AS Center for Language Education[D]^OR^AS Modern Languages and Literatures[D]^OR^H[A]^OR^S[A]",
+    },
+    {
+      name: "English Focused Writing Intensive",
+      required_credits: 3,
+      min_credits_per_course: 3,
+      double_count: true,
+      description:
+        "At least one course with a primary focus on writing in English must be chosen. " +
+        "Courses that satisfy this requirement are: EN.661.110, EN.661.111, EN.661.250, EN.661.251, " +
+        "EN.661.315, AS.060.100, AS.060.113, AS.220.105, AS.180.248, AS.290.303, AS.360.133.",
+      criteria:
+        "EN.661.110[C]^OR^EN.661.111[C]^OR^EN.661.250[C]^OR^EN.661.251[C]^OR^EN.661.315[C]" +
+        "^OR^AS.060.100[C]^OR^AS.060.113[C]^OR^AS.220.105[C]^OR^AS.180.248[C]^OR^AS.290.303[C]^OR^AS.360.133",
+    },
+  ],
+};
+
 // https://www.cs.jhu.edu/2021undergraduate-advising-manual/
 const bsCS_New: Major = {
-  degree_name: "B.S. Computer Science(NEW)",
+  degree_name: "B.S. Computer Science (NEW - 2021 & after)",
   department: "EN Computer Science",
   total_degree_credit: 120,
   wi_credit: 6,
