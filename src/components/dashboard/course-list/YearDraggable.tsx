cbd09223e42a712d6d44ab10152eb77d04007cf3
MattLiu-mygit <mliu78@jh.edu>
2021-10-22 01:38:21 -0400

    Revert "feat: years now scroll horizontally"
    This reverts commit 96b3a8639211155afdf61c45f1eb2089b11ded3b.

--- src/components/dashboard/course-list/YearDraggable.tsx
+++ src/components/dashboard/course-list/YearDraggable.tsx
@@ -59,7 +59,6 @@ const getItemStyle = (isDragging: any, draggableStyle: any) => ({
 
   padding: "0rem",
   marginRight: "1rem",
-  top: isDragging ? "-20" : "0",
 });
 
 export default YearDraggable;
