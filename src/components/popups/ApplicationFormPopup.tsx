e70f34c6226cd9643831da80c878ccd25481478f
Alan Zhang <azhang42@jh.edu>
2021-12-27 14:49:44 -0500

    fix: application form can now be accessed again

--- src/components/popups/ApplicationFormPopup.tsx
+++ src/components/popups/ApplicationFormPopup.tsx
@@ -29,7 +29,7 @@ const ApplicationFormPopup: FC<{
       {/* Actual popup */}
       <div
         className={
-          "shadow bg-primary z-40 fixed flex flex-col select-none rounded z-20 w-1/2 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 min-w-planAdd shadow"
+          "shadow bg-primary z-40 fixed flex flex-col select-none rounded w-1/2 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 min-w-planAdd shadow"
         }
       >
         <div className="px-4 py-2 text-white text-coursecard font-semibold select-none">
