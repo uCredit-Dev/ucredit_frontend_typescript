76d306c8233b5ddeeaaca75e8123357b1777e150
James S. Wang <JJamesWWang@gmail.com>
2022-03-30 17:29:16 -0400

    Playwright: Set up for testing with playwright init

---
+++ e2e/e2eFixtures.ts
@@ -0,0 +1,10 @@
+export const URL = 'http://localhost:3000/';
+
+export const HOME_PAGE = {
+  APP_NAME: 'uCredit',
+  LOGIN_BUTTON_SELECTOR: 'button:text("Log in")',
+};
+
+export const LOGIN_PAGE = {
+  LOGIN_TEXT_SELECTOR: 'button:text("JHU Login")',
+};
