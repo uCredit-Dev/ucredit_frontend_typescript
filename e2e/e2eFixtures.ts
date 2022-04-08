7f0834b81542bad4a9dbcee1439e2e22b9735c8b
James S. Wang <JJamesWWang@gmail.com>
2022-04-08 15:48:04 -0400

    E2E Testing: Fix login flow and creating new plan on login, as well as move landing page to separate file

--- e2e/e2eFixtures.ts
+++ e2e/e2eFixtures.ts
@@ -1,19 +1,26 @@
-export const URL = 'http://localhost:3000/';
+export const URL = 'http://localhost:3000';
+export const API_URL = 'http://localhost:4567';
+export const TEST_ID = 'TEST_DEV';
 
 export const HOME_PAGE = {
   APP_NAME: 'uCredit',
   LOGIN_BUTTON_SELECTOR: 'button:text("Log in")',
 };
 
-export const TEST_ID = "TEST_DEV";
-
 export const LOGIN_PAGE = {
   JHU_LOGIN_BUTTON_SELECTOR: 'button:text("JHU Login")',
-  LOGIN_INPUT_SELECTOR:
-    'input[placeholder="Enter your session ID cookie here"]',
-  CONFIRM_LOGIN_BUTTON_SELECTOR: 'button:text("Submit Custom Session")',
+  LOGIN_INPUT_SELECTOR: 'input[placeholder="Enter a custom ID here"]',
+  CONFIRM_LOGIN_BUTTON_SELECTOR: 'button:text("Login as custom user")',
+};
+
+export const ADD_PLAN_MODAL = {
+  ADD_PLAN_MODAL_SELECTOR: 'text=Adding a new plan!',
+  SELECT_MAJOR_INPUT_SELECTOR: 'id=react-select-7-input',
+  CS_BA_MAJOR_NAME: 'B.A. Computer',
+  ADD_MAJOR_BUTTON_SELECTOR: 'button:text("Add")',
 };
 
 export const DASHBOARD_PAGE = {
+  DEFAULT_PLAN_NAME_SELECTOR: 'text=Unnamed Plan',
   LOGOUT_BUTTON_SELECTOR: 'button:text("Log Out")',
-}
+};
--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,26 +1,25 @@
 import {
   URL,
+  API_URL,
   TEST_ID,
   HOME_PAGE,
   LOGIN_PAGE,
+  ADD_PLAN_MODAL,
   DASHBOARD_PAGE,
 } from './e2eFixtures';
 import { test, expect } from '@playwright/test';
+import axios from 'axios';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(URL);
 });
 
-test.describe('Landing Page', () => {
-  const { APP_NAME, LOGIN_BUTTON_SELECTOR } = HOME_PAGE;
-
-  test(`should have ${APP_NAME} text`, async ({ page }) => {
-    await expect(page.locator(`text=${APP_NAME}`).first()).toBeVisible();
-  });
-
-  test('should have Login button', async ({ page }) => {
-    await expect(page.locator(LOGIN_BUTTON_SELECTOR)).toBeVisible();
-  });
+test.afterEach(async () => {
+  try {
+    await axios.delete(`${API_URL}/api/user/${TEST_ID}`);
+  } catch (e) {
+    // Do nothing
+  }
 });
 
 test.describe('Login Page', () => {
@@ -34,7 +33,7 @@ test.describe('Login Page', () => {
     await expect(page.locator(JHU_LOGIN_BUTTON_SELECTOR)).toBeVisible();
   });
 
-  const login = async (page) => {
+  const after_login = async (page) => {
     const {
       JHU_LOGIN_BUTTON_SELECTOR,
       LOGIN_INPUT_SELECTOR,
@@ -46,15 +45,36 @@ test.describe('Login Page', () => {
   };
 
   test('should be able to login', async ({ page }) => {
-    await login(page);
-    await expect(page.locator(`text=${TEST_ID}`)).toBeVisible();
+    await after_login(page);
+    const { ADD_PLAN_MODAL_SELECTOR } = ADD_PLAN_MODAL;
+    await expect(page.locator(ADD_PLAN_MODAL_SELECTOR)).toBeVisible();
+  });
+
+  const after_plan_created = async (page) => {
+    await after_login(page);
+    const {
+      SELECT_MAJOR_INPUT_SELECTOR,
+      CS_BA_MAJOR_NAME,
+      ADD_MAJOR_BUTTON_SELECTOR,
+    } = ADD_PLAN_MODAL;
+    await page.locator(SELECT_MAJOR_INPUT_SELECTOR).type(CS_BA_MAJOR_NAME);
+    await page.locator(SELECT_MAJOR_INPUT_SELECTOR).press('Tab');
+    await page.locator(ADD_MAJOR_BUTTON_SELECTOR).click();
+  };
+
+  test('should be able to add the CS BA major', async ({ page }) => {
+    await after_plan_created(page);
+    const { DEFAULT_PLAN_NAME_SELECTOR } = DASHBOARD_PAGE;
+    await expect(
+      page.locator(DEFAULT_PLAN_NAME_SELECTOR).first(),
+    ).toBeVisible();
   });
 
   test('should be able to logout after logging in', async ({ page }) => {
-    await login(page);
+    await after_plan_created(page);
+
     const { LOGOUT_BUTTON_SELECTOR } = DASHBOARD_PAGE;
     const { JHU_LOGIN_BUTTON_SELECTOR } = LOGIN_PAGE;
-
     await page.click(LOGOUT_BUTTON_SELECTOR);
     await expect(page.locator(JHU_LOGIN_BUTTON_SELECTOR)).toBeVisible();
   });
