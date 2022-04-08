2c05c9549c05768a522e9603897cb670f56df071
James S. Wang <JJamesWWang@gmail.com>
2022-04-08 16:50:22 -0400

    E2E Login: Rearrange tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -22,48 +22,63 @@ test.afterEach(async () => {
   }
 });
 
+export const after_login_page = async (page) => {
+  const { LOGIN_BUTTON_SELECTOR } = HOME_PAGE;
+  await page.click(LOGIN_BUTTON_SELECTOR);
+};
+
+export const after_login = async (page) => {
+  await after_login_page(page);
+  const {
+    JHU_LOGIN_BUTTON_SELECTOR,
+    LOGIN_INPUT_SELECTOR,
+    CONFIRM_LOGIN_BUTTON_SELECTOR,
+  } = LOGIN_PAGE;
+  await page.click(JHU_LOGIN_BUTTON_SELECTOR);
+  await page.type(LOGIN_INPUT_SELECTOR, TEST_ID);
+  await page.click(CONFIRM_LOGIN_BUTTON_SELECTOR);
+};
+
+export const after_plan_created = async (page) => {
+  await after_login(page);
+  const {
+    SELECT_MAJOR_INPUT_SELECTOR,
+    CS_BA_MAJOR_NAME,
+    ADD_MAJOR_BUTTON_SELECTOR,
+  } = ADD_PLAN_MODAL;
+  await page.locator(SELECT_MAJOR_INPUT_SELECTOR).type(CS_BA_MAJOR_NAME);
+  await page.locator(SELECT_MAJOR_INPUT_SELECTOR).press('Tab');
+  await page.locator(ADD_MAJOR_BUTTON_SELECTOR).click();
+};
+
 test.describe('Login Page', () => {
   test.beforeEach(async ({ page }) => {
-    const { LOGIN_BUTTON_SELECTOR } = HOME_PAGE;
-    await page.click(LOGIN_BUTTON_SELECTOR);
+    await after_login_page(page);
   });
 
   test('should show jhu login button', async ({ page }) => {
     const { JHU_LOGIN_BUTTON_SELECTOR } = LOGIN_PAGE;
     await expect(page.locator(JHU_LOGIN_BUTTON_SELECTOR)).toBeVisible();
   });
+});
 
-  const after_login = async (page) => {
-    const {
-      JHU_LOGIN_BUTTON_SELECTOR,
-      LOGIN_INPUT_SELECTOR,
-      CONFIRM_LOGIN_BUTTON_SELECTOR,
-    } = LOGIN_PAGE;
-    await page.click(JHU_LOGIN_BUTTON_SELECTOR);
-    await page.type(LOGIN_INPUT_SELECTOR, TEST_ID);
-    await page.click(CONFIRM_LOGIN_BUTTON_SELECTOR);
-  };
+test.describe('Logging In', () => {
+  test.beforeEach(async ({ page }) => {
+    await after_login(page);
+  });
 
   test('should be able to login', async ({ page }) => {
-    await after_login(page);
     const { ADD_PLAN_MODAL_SELECTOR } = ADD_PLAN_MODAL;
     await expect(page.locator(ADD_PLAN_MODAL_SELECTOR)).toBeVisible();
   });
+});
 
-  const after_plan_created = async (page) => {
-    await after_login(page);
-    const {
-      SELECT_MAJOR_INPUT_SELECTOR,
-      CS_BA_MAJOR_NAME,
-      ADD_MAJOR_BUTTON_SELECTOR,
-    } = ADD_PLAN_MODAL;
-    await page.locator(SELECT_MAJOR_INPUT_SELECTOR).type(CS_BA_MAJOR_NAME);
-    await page.locator(SELECT_MAJOR_INPUT_SELECTOR).press('Tab');
-    await page.locator(ADD_MAJOR_BUTTON_SELECTOR).click();
-  };
+test.describe('Post-Login', () => {
+  test.beforeEach(async ({ page }) => {
+    await after_plan_created(page);
+  });
 
   test('should be able to add the CS BA major', async ({ page }) => {
-    await after_plan_created(page);
     const { DEFAULT_PLAN_NAME_SELECTOR } = DASHBOARD_PAGE;
     await expect(
       page.locator(DEFAULT_PLAN_NAME_SELECTOR).first(),
