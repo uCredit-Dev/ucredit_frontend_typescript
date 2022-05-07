b3e9550df80cf16c8adf48b0f87e30ee6e431903
Matthew Liu <mliu78@jh.edu>
2022-05-07 13:36:29 -0400

    fix: fixed elsint and prettier issues

--- lib/components/dashboard/menus/Notification.tsx
+++ lib/components/dashboard/menus/Notification.tsx
@@ -14,6 +14,7 @@ const Notification: FC<{
     (async () => {
       setNotifications((await userService.getNotifications(userID)).data);
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- lib/services/user.service.ts
+++ lib/services/user.service.ts
@@ -126,5 +126,5 @@ export const userService = {
   getThreads,
   postNewComment,
   changeReviewStatus,
-  getNotifications
+  getNotifications,
 };
