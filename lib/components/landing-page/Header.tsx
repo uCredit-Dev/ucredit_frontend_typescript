d67c806cce48dcf1052bed53e581ec1915d2ee5f
Rosa <ygao112@jhu.edu>
2022-06-15 10:26:43 -0400

    feat: finish header and footer skeleton

--- lib/components/landing-page/Header.tsx
+++ lib/components/landing-page/Header.tsx
@@ -1,5 +1,46 @@
 import { useRouter } from 'next/router';
 
+const Links: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <div className="space-x-5">
+      <button
+        className="px-3 py-1 hover:text-blue-header hover:bg-blue-footer rounded-[12px] transition duration-100 ease-in"
+        onClick={() => router.push('/about')}
+      >
+        About
+      </button>
+
+      <button
+        className="px-3 py-1 hover:text-blue-header hover:bg-blue-footer rounded-[12px] transition duration-100 ease-in"
+        onClick={() => router.push('/team')}
+      >
+        Team
+      </button>
+
+      <button
+        className="px-3 py-1 hover:text-blue-header hover:bg-blue-footer rounded-[12px] transition duration-100 ease-in"
+        onClick={() => router.push('/docs')}
+      >
+        Docs
+      </button>
+
+      <button
+        className="px-3 py-1 text-blue-header bg-blue-footer border hover:text-blue-footer hover:bg-blue-header border-blue-footer rounded-[12px] transition duration-100 ease-in"
+        onClick={() => router.push('/login')}
+      >
+        Log in
+      </button>
+
+    </div>
+  )
+}
+
+
+
+
+
 /**
  * Header of landing page.
  */
@@ -7,24 +48,29 @@ const Header: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div className="flex items-center justify-between p-4 h-1/6 bg-white">
-      <div className="flex items-center">
-        <img className="w-8 h-8 mr-2" src="/img/logo.png" alt="logo" />
-        <div
-          className="text-3xl italic font-bold cursor-pointer text-secondary"
-          onClick={() => router.push('/login')}
-        >
-          uCredit
-        </div>
+    <div className="sticky top-0 z-40 flex justify-between items-center py-1 px-4 h-1/6 bg-blue-header font-landingPage">
+      <div className="flex-grow">
+        
+          <div className="inline-flex">
+            <img className="w-12 h-12 mr-1 scale-x-[-1]" src="/img/logo.png" alt="logo" />
+                
+            <div
+              className="text-2xl cursor-pointer text-blue-footer self-center"
+              onClick={() => router.push('/login')}
+            >
+              uCredit
+            </div>
+          </div>
+      
       </div>
-      <button
-        className="px-2 py-0.5 hover:text-white hover:bg-black border border-gray-600 rounded-lg transition duration-100 ease-in"
-        onClick={() => router.push('/login')}
-      >
-        Log in
-      </button>
+      <div>
+        <Links />
+      </div>
+  
+      
     </div>
   );
 };
+
 
 export default Header;
