bea74ff4a67fd4014af8abeae7252c841d763306
Rosa <ygao112@jhu.edu>
2022-06-19 18:37:38 -0400

    feat: add team page

---
+++ pages/team.tsx
@@ -0,0 +1,16 @@
+import Header from '../lib/components/landing-page/Header';
+import Footer from '../lib/components/landing-page/Footer';
+
+
+const Team: React.FC = () => {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  )
+
+}
+
+export default Team;
