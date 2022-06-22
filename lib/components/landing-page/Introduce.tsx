0377bc781408a6e3360b6f2fc3f2453a0126ca77
QifanSun1128 <qsun17@jhu.edu>
2022-06-22 10:25:55 -0400

    feat: adjust blueJay & add wave

--- lib/components/landing-page/Introduce.tsx
+++ lib/components/landing-page/Introduce.tsx
@@ -1,19 +1,23 @@
 /**
  * Contains features and values that our app embraces.
  */
+ import Bird from './BlueJay.svg';
 import BgSVG from './BgSVG.svg';
 const Introduce: React.FC = () => {
   return (
   <>
-    <br></br><br></br><br></br><br></br>
-    <div className="font-landingPage  grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 items-stretch">
-    <div className="row-start-1 row-end-2 col-span-4 text-blue-footer text-center text-5xl">Introducing uCredit</div>
-    <div className="px-60 row-span-1 col-span-4 text-center text-xl">
-     A student-centered application packed with features like prereq checks, degree trackers, and beautiful UI that is easily shareable and accessible.
-    </div>
-    <br></br><br></br><br></br><br></br>
     
+    <br></br><br></br><br></br><br></br>
+    <div className="font-landingPage grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 items-stretch">
+      <div className="row-start-1 row-end-2 col-span-4 text-blue-footer text-center text-5xl">Introducing uCredit</div>
+      <div className="px-60 row-span-1 col-span-4 text-center text-xl">
+         A student-centered application packed with features like prereq checks, degree trackers, and beautiful UI that is easily shareable and accessible.
+      </div>
+      
     </div>
+    <img className="w-2/3 absolute left-[70vw] top-[110vh]" src="/img/logo.png" alt="logo"/>
+    <br></br><br></br><br></br> 
+    
   </>
  
   );
--- lib/components/landing-page/Promo.tsx
+++ lib/components/landing-page/Promo.tsx
@@ -1,14 +1,17 @@
 import data from './promoData';
 import BgSVG from './BgSVG.svg';
-import {ReactComponent as BGSVG} from './bg.svg'
+import {ReactComponent as BGSVG} from './BgSVG.svg'
+import Bird from './BlueJay.svg';
 
 /**
  * Contains features and values that our app embraces.
  */
 const Promo: React.FC = () => {
   return (
-
-    <div className="flex flex-col px-16 pb-8 my-10 text-center bg-blue-header text-blue-footer font-landingPage">
+    <>
+   
+    <img src="/img/landing-page/wave2.png" alt="cartoon schedule" />
+    <div className="flex flex-col px-16 pb-8  text-center bg-blue-header text-blue-footer font-landingPage">
       <div>
         {data.map((d) => {
           return !d.order ? (
@@ -17,7 +20,7 @@ const Promo: React.FC = () => {
             <div className="flex flex-row" key={d.title}>
               <div className="w-2/3 pt-5">
 
-              <div className="text-3xl mb-10 align-middle">{d.title}</div>
+              <div className="text-4xl mb-10 align-middle">{d.title}</div>
                 <div className="text-black">{d.desc}</div>
               </div>
 
@@ -34,8 +37,8 @@ const Promo: React.FC = () => {
                 <img className="rounded-md drop-shadow-2xl" alt="" src={d.img} />
               </div>
               <div className="w-2/3 p-5 align-middle">
-                <div className="text-3xl mb-10">{d.title}</div>
-                <div className="text-black mb-8">{d.desc}</div>
+                <div className="text-4xl mb-10">{d.title}</div>
+                <div className="text-black text-1xl mb-8">{d.desc}</div>
               </div>
             </div>
             </>
@@ -44,6 +47,7 @@ const Promo: React.FC = () => {
       </div>
     </div>
     
+    </>
   );
 
 };
