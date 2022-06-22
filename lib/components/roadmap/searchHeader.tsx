e636a940f8044cd9b4c88c66dbbbaa2aa81e704d
Matthew Flynn <matthewtf73@gmail.com>
2022-06-21 20:10:18 -0400

    added styling to navigation links

--- lib/components/roadmap/searchHeader.tsx
+++ lib/components/roadmap/searchHeader.tsx
@@ -3,7 +3,24 @@ import SearchBarArea from "./searchBarArea";
 const SearchHeader: React.FC = () => {
   return (
     <div className="sticky -top-48">
-      <div className="h-16 bg-red-300 sticky top-0">Navigation</div>
+      <div className="h-16 bg-primary sticky top-0 flex flex-row justify-center
+      items-center text-xl text-blue-900">
+        <img className="w-9 h-9 ml-5" src="/img/logo.png" alt="logo" />
+        <p className="flex-grow-0 mx-5">uCredit</p>
+        <p className="flex-grow"></p>
+        <a href="/" className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 underline
+        rounded-3xl hover:bg-blue-900 hover:text-white">
+          Dashboard
+        </a>
+        <a href="/" className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl 
+        text-white bg-blue-900">
+          Search
+        </a>
+        <a href="/" className="flex-grow-0 ml-2 mr-5 px-3 pt-0.5 pb-1 underline
+        rounded-3xl hover:bg-blue-900 hover:text-white">
+          Post
+        </a>
+      </div>
       <SearchBarArea/>
     </div>
   );
