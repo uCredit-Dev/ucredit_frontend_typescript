16b4630092be9b1fdfa063f08f03f7439bae4c36
Matthew Flynn <matthewtf73@gmail.com>
2022-06-24 17:00:24 -0400

    added header text above roadmap search bar

--- lib/components/roadmap/searchBarArea.tsx
+++ lib/components/roadmap/searchBarArea.tsx
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { RiMapPin2Fill } from 'react-icons/ri';
 import { updateSearchText } from '../../slices/roadmapSearchSlice';
 import SearchBar from './searchBar';
 
@@ -10,7 +11,11 @@ const SearchBarArea: React.FC = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-60 bg-white">
+    <div className="flex flex-col justify-center items-center h-60 bg-white">
+      <div className="flex flex-row">
+        <RiMapPin2Fill size={36} color="#94B6CC"/>
+        <h2 className="text-blue-900 text-4xl pl-2">uCredit Road Map</h2>
+      </div>
       <div className="sticky top-20 w-1/2">
         <SearchBar iconSize={28} onInputProp={onSearchInput} 
         placeHolder="Search" heightClass='h-12' iconPosition='left-3'/>
