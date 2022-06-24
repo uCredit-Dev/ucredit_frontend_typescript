cd1d7ffee5852c9c29696b6df8233085ee3fcfab
Cloud9-ine <cloooud9.1997@gmail.com>
2022-06-24 19:40:52 -0400

    Update SearchResultCard

---
+++ lib/components/roadmap/searchResultCard.tsx
@@ -0,0 +1,105 @@
+import React, {FC} from 'react';
+import {AiOutlineComment, AiFillEye, AiOutlineLike, AiOutlineStar} from "react-icons/ai";
+import {Avatar, Badge, Card, Divider, Tag} from "antd";
+
+// The parameter (planInfo) remains to be filled.
+// The input planInfo should at least include to be rendered:
+// "planName,
+// planId,
+// uploadDate,
+// tagsList[] (each tag should have a name and corresponding color),
+// content,
+// watchNum,
+// likeNum,
+// starNum,
+// commentNum".
+
+const SearchResultCard: React.FC = () => {
+    return (
+        <div className="bg-blue-200 mx-5 mt-5 mb-5">
+
+            <div className="grid grid-cols-2 gap-4 place-content-between h-20 ...">
+                {/*title*/}
+                <div>
+                    <h1 className="text-4xl flex-grow-0 mx-5 pt-3 pb-1 font-semibold"> Plan Name </h1>
+                </div>
+
+                <div>
+                    <div className="flex justify-end mx-5 pt-3 pb-1">
+                        {/*Date*/}
+                        <div>
+                            <p>Uploaded on 2022-06-04</p>
+                        </div>
+                    </div>
+
+                    {/*watchNum*/}
+                    <div className="flex justify-end mx-5 pt-3 pb-1">
+                        <a className="flex-grow-0 pb-1bg-blue-900">
+                            <div className="flex flex-row">
+                                <AiFillEye/>
+                                <p>324</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            {/*tagsList*/}
+            <div className="flex flex-row">
+                <div>
+                    <a className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl
+        text-blue bg-yellow-300">
+                        English
+                    </a>
+                </div>
+                <div>
+                    <a className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl
+        text-blue bg-green-300">
+                        Humanity
+                    </a>
+                </div>
+
+                <div>
+                    <a className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl
+        text-blue bg-pink-300">
+                        Pre-law
+                    </a>
+                </div>
+            </div>
+
+            {/*Contents*/}
+            <div className="flex-grow-0 mx-2 px-3 pt-3 pb-3">
+                <p className="flex-grow font-semibold text-left">a brief description of this plan,
+                    such as the main purpose and concentration.
+                    two lines of preview in total ...
+                </p>
+            </div>
+
+            <div className="flex justify-end">
+                {/*Stars*/}
+                <a href="/" className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1bg-blue-900">
+                    <div className="flex flex-row">
+                        <AiOutlineStar/>
+                    </div>
+                </a>
+                {/*Likes*/}
+                <a href="/" className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1bg-blue-900">
+                    <div className="flex flex-row">
+                        <AiOutlineLike/>
+                        <p>102</p>
+                    </div>
+                </a>
+                {/*Comments*/}
+                <a href="/" className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1bg-blue-900">
+                    <div className="flex flex-row">
+                        <AiOutlineComment/>
+                        <p>12</p>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    );
+}
+
+export default SearchResultCard;
--- pages/roadmap/search.tsx
+++ pages/roadmap/search.tsx
@@ -1,5 +1,6 @@
 import SearchHeader from "../../lib/components/roadmap/searchHeader";
 import SearchDetailPane from "../../lib/components/roadmap/searchDetailPane";
+import SearchResultCard from "../../lib/components/roadmap/searchResultCard";
 
 const RoadmapSearch: React.FC = () => {
   return (
@@ -11,9 +12,13 @@ const RoadmapSearch: React.FC = () => {
         </div>
         {/* The bg color and height can be removed when the SearchResultCard
         components are added (they're just for testing/layout purposes) */}
+
         <div className="basis-3/5 bg-sky-200 h-[120vh]">
-          Search Results
+          {/*Search Results*/}
           {/* List of SearchResultCard components goes here */}
+            <SearchResultCard/>
+
+            <SearchResultCard/>
         </div>
       </div>
     </>
