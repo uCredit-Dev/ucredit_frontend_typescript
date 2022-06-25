918fe10195a13cc8f13dd641c1921f2d5317f7eb
Matthew Flynn <matthewtf73@gmail.com>
2022-06-24 20:03:45 -0400

    small layout changes

--- pages/roadmap/search.tsx
+++ pages/roadmap/search.tsx
@@ -6,18 +6,19 @@ const RoadmapSearch: React.FC = () => {
   return (
     <>
       <SearchHeader/>
-      <div className="flex flex-row">
-        <div className="basis-2/5 bg-white">
+      <div className="flex flex-row bg-white">
+        <div className="basis-2/5">
           <SearchDetailPane/>
         </div>
         {/* The bg color and height can be removed when the SearchResultCard
         components are added (they're just for testing/layout purposes) */}
 
-        <div className="basis-3/5 bg-sky-200 h-[120vh]">
+        <div className="basis-3/5 [120vh]">
           {/*Search Results*/}
           {/* List of SearchResultCard components goes here */}
             <SearchResultCard/>
-
+            <SearchResultCard/>
+            <SearchResultCard/>
             <SearchResultCard/>
         </div>
       </div>
