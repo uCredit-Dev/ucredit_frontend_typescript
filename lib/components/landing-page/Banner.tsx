fdd956419b2f2c0746dcd4960ffa17e7d133aac1
Matthew Liu <mliu78@jh.edu>
2022-06-24 17:26:17 -0700

    fix: fixed bird width and top values for windows laptop screen

--- lib/components/landing-page/Banner.tsx
+++ lib/components/landing-page/Banner.tsx
@@ -8,58 +8,77 @@ const Laptop: React.FC = () => {
   const router = useRouter();
 
   return (
-      <div className="grid flex grid-cols-9 h-30 bg-blue-header items-stretch font-landingPage">
-        
-        <div className="p-35 col-span-4">
-          <div className="grid grid-flow-row rows">
-            <div className="p-8 mb-2 font-medium m-5 text-3xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-right text-name">uCredit</div>
-            <div className="p-3 mb-2 font-normal m-2 text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-4xl text-right pr-10 text-slogan text-opacity-70">
-              Your Academic Planning, <br></br> Made Simple
-            </div>
-            <Bird className="z-10 mr-1 scale-[60%]  lg:scale-75 xl:scale-100 md:top-[22%] ml:top-[24rem] lg:top-[80%] top-[20%]  md:left-[-30%] lg:left-[-10%] xl:left-0 left-[-40%] absolute" alt="logo" />
-            <div className="relative z-10 p-4 mb-2 text-right border-blue-footer pr-16 pt-5">
-              {/* <img className=" scale-x-[-1] absolute" src="/img/logo.png" alt="logo" /> */}
-              <button className="z-40 bg-blue-footer text-white absolute font-normal text-sm py-4 px-10 sm:text-base md:text-lg lg:text-xl xl:text-2xl rounded-full right-8" onClick={() => router.push('/login')}>
-                Start
-              </button>
-            </div> 
-          </div> 
+    <div className="grid flex grid-cols-9 h-30 bg-blue-header items-stretch font-landingPage">
+      <div className="p-35 col-span-4">
+        <div className="grid grid-flow-row rows">
+          <div className="p-8 mb-2 font-medium m-5 text-3xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-right text-name">
+            uCredit
+          </div>
+          <div className="p-3 mb-2 font-normal m-2 text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-4xl text-right pr-10 text-slogan text-opacity-70">
+            Your Academic Planning, <br></br> Made Simple
+          </div>
+          <Bird
+            className="z-10 mr-1 scale-[60%]  lg:scale-75 xl:scale-100 md:top-[22%] ml:top-[25rem] lg:top-[80%] top-[20%]  md:left-[-30%] lg:left-[-10%] xl:left-0 left-[-40%] absolute"
+            alt="logo"
+          />
+          <div className="relative z-10 p-4 mb-2 text-right border-blue-footer pr-16 pt-5">
+            {/* <img className=" scale-x-[-1] absolute" src="/img/logo.png" alt="logo" /> */}
+            <button
+              className="z-40 bg-blue-footer text-white absolute font-normal text-sm py-4 px-10 sm:text-base md:text-lg lg:text-xl xl:text-2xl rounded-full right-8"
+              onClick={() => router.push('/login')}
+            >
+              Start
+            </button>
+          </div>
         </div>
-        <div className="p-4 col-span-5 w-5/6">
-          <img src="/img/landing-page/cartoon.png" alt="cartoon schedule" />
-          <div className="flex items-right justify-end mt-2 text-gray-600 select-none sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-            Partnered with Semesterly
+      </div>
+      <div className="p-4 col-span-5 w-5/6">
+        <img src="/img/landing-page/cartoon.png" alt="cartoon schedule" />
+        <div className="flex items-right justify-end mt-2 text-gray-600 select-none sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+          Partnered with Semesterly
           <img
             src="/img/semlylogo.png"
             alt="semesterly"
             className="w-8 h-8 ml-1"
           />
-          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const Mobile: React.FC = () => {
   const router = useRouter();
 
   return (
-      <div className="bg-blue-header content-center items-center font-landingPage text-center">
-        <div className="text-2xl font-bold text-blue-footer">Your Academic Planning <br></br> Made Simple</div>
-        <br></br>
-        <img className="h-62 px-12" src="/img/landing-page/cartoon.png" alt="cartoon schedule" />
-        <br></br>
-        <div className="text-xs text-blue-footer px-8">uCredit is a degree-tracking application packed with unique features, 
-          where users seamlessly manage their academic plans and developers are empowered to build great student-centered tools.
-        </div>
-        <br></br><br></br>
-        <button className="bg-blue-footer text-white absolute font-normal text-xl py-4 px-20 rounded-full right-8 left-8 text-center" onClick={() => router.push('/login')}>
-          Start Planning
-        </button>
-        <br></br>
+    <div className="bg-blue-header content-center items-center font-landingPage text-center">
+      <div className="text-2xl font-bold text-blue-footer">
+        Your Academic Planning <br></br> Made Simple
       </div>
-  )
-}
+      <br></br>
+      <img
+        className="h-62 px-12"
+        src="/img/landing-page/cartoon.png"
+        alt="cartoon schedule"
+      />
+      <br></br>
+      <div className="text-xs text-blue-footer px-8">
+        uCredit is a degree-tracking application packed with unique features,
+        where users seamlessly manage their academic plans and developers are
+        empowered to build great student-centered tools.
+      </div>
+      <br></br>
+      <br></br>
+      <button
+        className="bg-blue-footer text-white absolute font-normal text-xl py-4 px-20 rounded-full right-8 left-8 text-center"
+        onClick={() => router.push('/login')}
+      >
+        Start Planning
+      </button>
+      <br></br>
+    </div>
+  );
+};
 
 const Banner: React.FC = () => {
   const [activateEmailPopup, setActivateEmailPopup] = useState<boolean>(false);
@@ -71,20 +90,24 @@ const Banner: React.FC = () => {
         <ApplicationFormPopup setActivateEmailPopup={setActivateEmailPopup} />
       )}
       <div className=" bg-blue-header ">
-      <br></br><br></br>
-      
-      
-      <div className="hidden sm:block"><Laptop /></div>
-      <div className="block sm:hidden"><Mobile /></div>
-      <br></br><br></br>
+        <br></br>
+        <br></br>
+
+        <div className="hidden sm:block">
+          <Laptop />
+        </div>
+        <div className="block sm:hidden">
+          <Mobile />
+        </div>
+        <br></br>
+        <br></br>
       </div>
-    <img src="/img/landing-page/wave.png" alt="cartoon schedule" />
-          
-    <div
-      className="w-full cursor-pointer"
-      onClick={() => window.scrollTo(0, 575)}
-    >
-    </div>
+      <img src="/img/landing-page/wave.png" alt="cartoon schedule" />
+
+      <div
+        className="w-full cursor-pointer"
+        onClick={() => window.scrollTo(0, 575)}
+      ></div>
     </>
   );
 };
--- lib/components/landing-page/Introduce.tsx
+++ lib/components/landing-page/Introduce.tsx
@@ -1,63 +1,76 @@
 /**
  * Contains features and values that our app embraces.
  */
-import Bird from './BlueJay.svg';
-import BgSVG from './BgSVG.svg';
 import data from './introduceData';
 
 const Laptop: React.FC = () => {
   return (
     <div className="">
-      <br></br><br></br><br></br><br></br>
-      <img className="w-2/3 absolute left-[70vw] top-[110vh]" src="/img/logo.png" alt="logo" />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <img
+        className="w-2/3 absolute left-[70vw] top-[115vh] z-[0]"
+        src="/img/logo.png"
+        alt="logo"
+      />
       <div className="font-landingPage grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 items-stretch">
-        <div className="row-start-1 row-end-2 col-span-4 text-blue-footer text-center text-5xl">Introducing uCredit</div>
-        <div className="px-60 row-span-1 col-span-4 text-center text-xl">
-          A student-centered application packed with features like prereq checks, degree trackers, and beautiful UI that is easily shareable and accessible.
+        <div className="row-start-1 row-end-2 col-span-4 text-blue-footer text-center text-5xl">
+          Introducing uCredit
+        </div>
+        <div className="px-60 row-span-1 col-span-4 text-center text-xl z-20">
+          A student-centered application packed with features like prereq
+          checks, degree trackers, and beautiful UI that is easily shareable and
+          accessible.
         </div>
       </div>
-
-
     </div>
-
   );
 };
 
 const Mobile: React.FC = () => {
   return (
-
     <div className="grid grid-cols-1 content-center items-center font-landingPage font-bold text-blue-footer text-left ml-10 py-5">
       <div className="text-2xl ">Our Features</div>
-      <img className="w-2/3 absolute left-[60vw] top-[65vh]" src="/img/logo.png" alt="logo" />
+      <img
+        className="w-2/3 absolute left-[60vw] top-[65vh]"
+        src="/img/logo.png"
+        alt="logo"
+      />
       <div>
         {data.map((d) => {
           return (
             <>
-            <br></br>
-            <div className="flex items-center flex-column align-middle" key={d.title}>
-              <div className="flex bg-gray-200 rounded-md ">
-                <img className="rounded-md" alt="" src={d.img} />
+              <br></br>
+              <div
+                className="flex items-center flex-column align-middle"
+                key={d.title}
+              >
+                <div className="flex bg-gray-200 rounded-md ">
+                  <img className="rounded-md" alt="" src={d.img} />
+                </div>
+                <div className="flex p-2">
+                  <div className="text-l text-blue-footer">{d.title}</div>
+                </div>
               </div>
-              <div className="flex p-2">
-                <div className="text-l text-blue-footer">{d.title}</div>
-              </div>
-              
-            </div>
             </>
-          )
+          );
         })}
       </div>
     </div>
-
   );
 };
-
 
 const Introduce: React.FC = () => {
   return (
     <>
-      <div className="hidden sm:block"><Laptop /></div>
-      <div className="block sm:hidden"><Mobile /></div>
+      <div className="hidden sm:block">
+        <Laptop />
+      </div>
+      <div className="block sm:hidden">
+        <Mobile />
+      </div>
     </>
   );
 };
