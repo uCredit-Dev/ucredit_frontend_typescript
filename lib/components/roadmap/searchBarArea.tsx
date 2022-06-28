fdfb7357956eda07fc1005b37ecbcb7f8d9fbb63
Matthew Flynn <matthewtf73@gmail.com>
2022-06-28 16:45:30 -0400

    updated responsive search bar size

--- lib/components/roadmap/searchBarArea.tsx
+++ lib/components/roadmap/searchBarArea.tsx
@@ -13,12 +13,13 @@ const SearchBarArea: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center h-60 bg-white">
       <div className="flex flex-row">
-        <RiMapPin2Fill size={36} color="#94B6CC"/>
+        <RiMapPin2Fill size={36} color="#94B6CC" className='hidden md:block'/>
         <h2 className="text-blue-900 text-4xl pl-2">uCredit Road Map</h2>
       </div>
-      <div className="sticky top-20 w-1/2">
+      <div className="sticky top-20 w-5/6 md:w-1/2">
         <SearchBar iconSize={28} onInputProp={onSearchInput} 
-        placeHolder="Search" heightClass='h-12' iconPosition='left-3'/>
+        placeHolder="Search" heightClass='h-10 md:h-12' 
+        iconPosition='left-2 md:left-3'/>
       </div>
     </div>
   );
