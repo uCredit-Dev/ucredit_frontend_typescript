e7ff8869264f6385ad9a4dbd40cc20ff60e0227c
Matthew Flynn <matthewtf73@gmail.com>
2022-06-28 16:15:53 -0400

    toggle for roadmap search details on mobile

--- pages/roadmap/search.tsx
+++ pages/roadmap/search.tsx
@@ -1,16 +1,45 @@
 import SearchHeader from "../../lib/components/roadmap/searchHeader";
 import SearchDetailPane from "../../lib/components/roadmap/searchDetailPane";
 import SearchResultCard from "../../lib/components/roadmap/searchResultCard";
+import { useState } from "react";
 
 const RoadmapSearch: React.FC = () => {
+  const [advToolsClass, setAdvToolsClass] = useState("hidden");
+
+  const showAdvTools = () => {
+    setAdvToolsClass("");
+  }
+
+  const hideAdvTools = () => {
+    setAdvToolsClass("hidden");
+  }
+
+  const showToolsButtonClass = () => {
+    if (advToolsClass === "hidden") {
+      return "";
+    } else {
+      return "hidden";
+    }
+  }
+
   return (
     <>
       <SearchHeader/>
-      <div className="flex flex-row bg-white">
-        <div className="basis-2/5">
-          <SearchDetailPane/>
+      <div className="flex flex-col md:flex-row bg-white">
+        <div className="md:basis-2/5">
+          <button className={`text-center underline text-blue-600
+          w-full md:hidden ${showToolsButtonClass()}`} onClick={showAdvTools}>
+            Show advanced search tools
+          </button>
+          <div className={`md:block ${advToolsClass}`}>
+            <button className="text-center underline 
+            text-blue-600 w-full md:hidden" onClick={hideAdvTools}>
+              Hide advanced search tools
+            </button>
+            <SearchDetailPane/>
+          </div>
         </div>
-        <div className="basis-3/5">
+        <div className="md:basis-3/5">
           <SearchResultCard/>
           <SearchResultCard/>
           <SearchResultCard/>
