ba682c1de143ad93e18448e5ce80c692575df3fd
XuhuaSun <cloooud9.1997@gmail.com>
2022-07-01 19:55:41 -0400

    Update searchDetailPane.tsx

--- lib/components/roadmap/searchDetailPane.tsx
+++ lib/components/roadmap/searchDetailPane.tsx
@@ -1,26 +1,45 @@
 import SearchBar from "./searchBar";
+import {useState} from "react";
 
 const SearchDetailPane: React.FC = () => {
-  return (
+    const [selectedMajor, setselectedMajor] = useState("");
+    const majorArray = [
+        "B.S. in Computer Science",
+        "B.A. in Computer Science",
+        "B.S. in Electrical Engineering",
+        "B.A. in Mathematics",
+        "B.S. in Public Health",
+        "B.A. in Something Else",
+        "B.S. in Something Else",
+    ];
+
+    const changeSelectedMajor = (evt) => {
+        setselectedMajor(evt.target.innerText);
+    };
+
+    const onInputChange = (evt) => {
+        setselectedMajor(evt.target.value);
+    };
+
+    return (
     <div className="mx-8 my-5 px-6 py-4 bg-sky-100 rounded-xl">
-      <SearchBar iconSize={24} onInputProp={() => {}} 
+      <SearchBar iconSize={24} onInputProp={() => {}}
       placeHolder="Search tags" heightClass="h-10" iconPosition="left-16"/>
       <div className="w-full my-2 outline outline-gray-600 outline-2
       rounded-2xl overflow-hidden bg-white">
-        <input className="w-full h-10 px-3 rounded-t-2xl outline 
+        <input className="w-full h-10 px-3 rounded-t-2xl outline
         outline-gray-600 outline-2 mb-[2px]"
-        type='text' placeholder="search or select major"/>
+        type='text' placeholder="search or select major" value={selectedMajor}
+               onChange={onInputChange}/>
         <ul className="flex flex-col h-40 overflow-y-scroll rounded-b-2xl
         child:py-2 child:bg-white child:w-full child:text-left child:px-3
         child:outline child:outline-gray-400 child:outline-1 
         hover:child:underline hover:child:bg-gray-200">
-          <button>B.S. in Computer Science</button>
-          <button>B.A. in Computer Science</button>
-          <button>B.S. in Electrical Engineering</button>
-          <button>B.A. in Mathematics</button>
-          <button>B.S. in Public Health</button>
-          <button>B.A. in Something Else</button>
-          <button>B.S. in Something Else</button>
+          {majorArray.map((item) => {
+              if (item.includes(selectedMajor)) {
+                  return (<button onClick={changeSelectedMajor}>{item}</button>);
+              }
+          })}
         </ul>
       </div>
       <p className="relative left-2 mt-4">
@@ -30,5 +49,5 @@ const SearchDetailPane: React.FC = () => {
     </div>
   );
 }
-  
+
 export default SearchDetailPane;
