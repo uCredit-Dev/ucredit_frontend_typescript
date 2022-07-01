d048e66d417153e755864f86bf9c2dab65caf915
Matthew Flynn <matthewtf73@gmail.com>
2022-07-01 19:40:32 -0400

    roadmap search collapsing nav on mobile

--- lib/components/roadmap/searchHeader.tsx
+++ lib/components/roadmap/searchHeader.tsx
@@ -1,8 +1,29 @@
 import SearchBarArea from "./searchBarArea";
 import { selectMobileAdvSearch } from "../../slices/roadmapSearchSlice";
+import { useState } from "react";
 import { useSelector } from "react-redux";
+import { FaBars } from 'react-icons/fa';
+import { GrClose } from 'react-icons/gr';
 
 const SearchHeader: React.FC = () => {
+  const [mobileNavShowing, setMobileNavShowing] = useState(false);
+
+  const getMobileNavClass = () => {
+    if (mobileNavShowing) {
+      return "";
+    } else {
+      return "hidden";
+    }
+  }
+
+  const oppositeMobileNavClass = () => {
+    if (mobileNavShowing) {
+      return "hidden";
+    } else {
+      return "";
+    }
+  }
+
   const mobileAdvSearch = useSelector(selectMobileAdvSearch);
 
   const getOuterTopClass = () => {
@@ -15,27 +36,49 @@ const SearchHeader: React.FC = () => {
 
   return (
     <div className={`sticky ${getOuterTopClass()} md:-top-36`}>
-      <div className="h-16 bg-sky-200 sticky top-0 flex flex-row justify-center
-      items-center text-lg text-blue-900">
+      <div className="h-16 bg-white md:bg-sky-200 sticky top-0 flex flex-row
+      justify-center items-center text-lg text-blue-900">
         <a href="/">
           <div className="flex flex-row">
             <img className="w-9 h-9 ml-5" src="/img/logo.png" alt="logo" />
-            <p className="text-xl flex-grow-0 mx-5 pt-0.5 pb-1">uCredit</p>
+            <p className="hidden md:block text-xl flex-grow-0 mx-5 pt-0.5
+            pb-1">
+              uCredit
+            </p>
           </div>
         </a>
         <p className="flex-grow"></p>
-        <a href="/" className="flex-grow-0 mx-2 px-3 pb-0.5 underline
-        rounded-3xl hover:bg-blue-900 hover:text-white">
-          Dashboard
-        </a>
-        <a href="/" className="flex-grow-0 mx-2 px-3 pb-0.5 rounded-3xl 
-        text-white bg-blue-900">
-          Search
-        </a>
-        <a href="/" className="flex-grow-0 ml-2 mr-5 px-3 pb-0.5 underline
-        rounded-3xl hover:bg-blue-900 hover:text-white">
-          Post
-        </a>
+        <div className="relative">
+          <button className={`${oppositeMobileNavClass()}`}
+          onClick={() => setMobileNavShowing(true)}>
+            <FaBars size={28} className="md:hidden mr-6"/>
+          </button>
+          <button className={`${getMobileNavClass()}`}
+          onClick={() => setMobileNavShowing(false)}>
+            <GrClose size={28} className="md:hidden mr-6"/>
+          </button>
+          <div className={`absolute right-6 flex flex-col w-max outline
+          outline-2 outline-black rounded-2xl overflow-hidden child:px-4 
+          child:py-1 child:outline child:outline-gray-400 child:outline-1
+          ${getMobileNavClass()}`}>
+            <a href="/">Dashboard</a>
+            <a href="/">Post a Plan</a>
+          </div>
+        </div>
+        <div className="hidden md:block flex-grow-0">
+          <a href="/" className="mx-2 px-3 pb-0.5 underline rounded-3xl 
+          hover:bg-blue-900 hover:text-white">
+            Dashboard
+          </a>
+          <a href="/" className="mx-2 px-3 pb-0.5 rounded-3xl text-white 
+          bg-blue-900">
+            Search
+          </a>
+          <a href="/" className="ml-2 mr-5 px-3 pb-0.5 underline rounded-3xl 
+          hover:bg-blue-900 hover:text-white">
+            Post
+          </a>
+        </div>
       </div>
       <SearchBarArea/>
     </div>
