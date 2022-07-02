8b4cb99906ea62f879df83b61281f87f2258c10f
QifanSun1128 <qsun17@jhu.edu>
2022-07-01 21:39:55 -0400

    feat: polish Card

--- lib/components/landing-page/team-page/Card.tsx
+++ lib/components/landing-page/team-page/Card.tsx
@@ -14,13 +14,13 @@ function Card(props: TeamMemberType) {
   const { img, name, role } = props;
 
   return (
-    <div className="flex w-[320px] h-[200px] bg-white shadow-card rounded-lg py-1 pl-3 text-black font-landingPage">
+    <div className="flex w-[350px] h-[200px] bg-white shadow-lg rounded-lg py-1 pl-3 text-black font-landingPage">
       <img
-        className="w-40 h-40 my-4 rounded-[15px] object-cover"
+        className="w-[125px] my-4 mx-2 rounded-[30px] object-cover"
         src={img}
         alt="profile"
       />
-      <div className="w-48 px-5 py-5">
+      <div className="w-48 px-4 py-7">
         <div className="flex flex-col">
           <div>
             <p className="text-2xl text-left">{name}</p>
@@ -30,7 +30,7 @@ function Card(props: TeamMemberType) {
             )}
           </div>
 
-          <div>
+          <div className="-mx-2 my-7">
             <Links linkedin={`${props.linkedin}`} github={`${props.github}`} portfolio={`${props.portfolio}`} />
           </div>
         </div>
