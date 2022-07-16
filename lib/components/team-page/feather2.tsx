d7838347595ab12a3d97e35f137a50d1e5a42655
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-07-16 12:20:25 -0400

    Fix about svgs and fix course selection
    1. I put the new svgs in the About page, and made them mobile friendly.
    2. I also fixed an issue with a selected course not appearing when selected from the degree progress menu on mobile.

--- lib/components/team-page/feather2.tsx
+++ lib/components/team-page/feather2.tsx
@@ -1,7 +1,7 @@
 function feather2() {
   return (
     <svg
-      className="sm:h-[72px] sm:w-[72px] pt-1 h-[42px] w-[42px] mr-2 sm:mr-20"
+      className="sm:h-[72px] sm:w-[72px] pt-1 h-[42px] w-[42px] mr-2 sm:mr-12"
       width="76"
       height="76"
       viewBox="0 0 76 76"
