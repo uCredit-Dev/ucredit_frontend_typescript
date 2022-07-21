cad5556457ffe3f296af1baab4a192f11c1c1356
QifanSun1128 <qsun17@jhu.edu>
2022-07-21 11:53:34 -0400

    fix: move banner, header, comment of roadmap

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import CommentIcon from "../lib/components/roadmap-page/commentEditor/CommentIcon";
 import Editor from "../lib/components/roadmap-page/commentEditor/Editor";
-
+import Header from '../lib/components/roadmap/roadMapHeader';
+import RoadMapComment from '../lib/components/roadmap/roadMapComment';
+import Banner from '../lib/components/roadmap/roadMapBanner';
 
 const RoadMap: React.FC = () => {
   const [editorPopup, setEditorPopup] = useState(false);
@@ -16,23 +18,32 @@ const RoadMap: React.FC = () => {
     setEditorPopup(false);
   }
 
-
   const addComments = (newComment: string) => {
     setComments([...comments, newComment]);
   }
 
   return (
     <>
+    <div className="font-roadMapPage">
+      <Header/>
+      <Banner/>
+      <RoadMapComment/>
+
       <div className="absolute bottom-10 right-10">
         <CommentIcon openEditor={openEditor}/>
       </div>
 
+      
+
       <div>
         {editorPopup && <Editor addComments={addComments} closeEditor={closeEditor}/>}
+      </div>
       </div>
       
     </>
   )
+
+  
 }
 
 export default RoadMap;
