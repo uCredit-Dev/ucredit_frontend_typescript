519b59218e94752170626359e1b9bfd84a7eb8c2
QifanSun1128 <qsun17@jhu.edu>
2022-07-21 21:12:42 -0400

    feat: add icons to roadmap page

--- lib/components/roadmap/roadMapBanner.tsx
+++ lib/components/roadmap/roadMapBanner.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import {
+  AiOutlineComment,
+  AiFillEye,
+  AiOutlineLike,
+  AiOutlineStar,
+} from 'react-icons/ai';
+import Star from './assets/Star.svg'; 
+import Like from './assets/Like.svg';
+import View from './assets/View.svg';
 
 const Laptop: React.FC = () => {
   const router = useRouter();
@@ -7,11 +16,61 @@ const Laptop: React.FC = () => {
     <>
       <div className="flex flex-row bg-sky-100 items-stretch w-full">
         <div className="mx-24 my-9 text-left">
-          <div className="text-3xl sm: md:text- lg: xl: xxxl: ">
-            The Best Plan for CS majors (plan name)
+          <div className="felx flex-row">
+            {/* plan Name */}
+            <div className="flex flex-row text-3xl sm: md:text- lg: xl: xxxl: ">
+              The Best Plan for CS majors (plan name)
+            </div>
+            <div className="flex flex-row ml-6">
+              {/*Stars*/}
+              <button className="flex flex-grow-0 items-center mx-2 px-3">
+                <Star />
+              </button>
+
+              {/*Likes*/}
+              <button className="flex flex-grow-0 items-center mx-2 px-3">
+                <Like />
+                <p>102</p>
+              </button>
+
+              {/*Comments*/}
+              <div className="flex flex-row items-center mx-2 px-3">
+                <View />
+                <p>324</p>
+              </div>
+            </div>
           </div>
-          <div className="text-sm mt-2 sm: md: lg: xl: ">by uCredit</div>
-          <div className="text-sm mb-4 sm: md: lg: xl: ">uploaded on 2022-07-20</div>
+
+          <div className="flex flex-row">
+            {/* author */}
+            <div className="text-sm mt-3 sm: md: lg: xl: ">by uCredit</div>
+            {/*tagsList*/}
+            <div className="flex flex-row ml-64 mt-2">
+              <div>
+                <button className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl text-blue bg-yellow-tag">
+                  English
+                </button>
+              </div>
+              <div>
+                <button
+                  className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl text-blue bg-green-tag">
+                  Humanity
+                </button>
+              </div>
+
+              <div>
+                <button
+                  className="flex-grow-0 mx-2 px-3 pt-0.5 pb-1 rounded-3xl text-blue bg-red-tag">
+                  Pre-law
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* date */}
+          <div className="text-sm mb-4 sm: md: lg: xl: ">
+            uploaded on 2022-07-20
+          </div>
           <div className="text-sm sm: md: lg: xl: ">
             description of the plan. like pros and cons, explanantion for each
             semester etc... can be a loooong description there’s plenty of space
