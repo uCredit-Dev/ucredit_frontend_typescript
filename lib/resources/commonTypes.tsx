328c6b4ae836c8fcb469311f058ebaa14eb09bd9
mpark63 <minspk13@gmail.com>
2022-07-24 14:02:20 +0900

    exclusive is now double_count

--- lib/resources/commonTypes.tsx
+++ lib/resources/commonTypes.tsx
@@ -159,7 +159,6 @@ export type Distribution = {
   criteria: string;
   fine_requirements?: FineReq[];
   user_select?: boolean;
-  double_count?: boolean;
   exception?: string;
   planned_credits: number;
   courses: string[];
@@ -194,7 +193,7 @@ export type FineReq = {
   required_credits: number;
   description: string;
   criteria: string;
-  exclusive?: string[];
+  double_count?: string[];
 };
 
 export type DistributionObj = {
@@ -205,9 +204,8 @@ export type DistributionObj = {
   criteria: string;
   fine_requirements?: FineReq[];
   user_select?: boolean;
-  double_count?: boolean;
+  double_count?: string[];
   exception?: string;
-  exclusive?: string[];
   pathing?: number;
 };
 
