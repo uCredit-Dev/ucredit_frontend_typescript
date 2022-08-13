7999a838f5199daa4e289724a9c329bfd8223ee2
Matthew Liu <mliu78@jh.edu>
2022-08-13 00:51:03 -0700

    fix: fixed prettier

--- lib/components/dashboard/course-list/YearComponent.tsx
+++ lib/components/dashboard/course-list/YearComponent.tsx
@@ -362,7 +362,10 @@ const YearComponent: FC<{
       id={id.toString()}
       className={clsx(
         'max-w-year-heading w-full min-w-[14rem]',
-        { 'cursor-move': !mode || (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap)},
+        {
+          'cursor-move':
+            !mode || (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap),
+        },
         { 'z-30': addingPrereqStatus },
       )}
       onMouseLeave={() => {
@@ -402,7 +405,11 @@ const YearComponent: FC<{
               id={year._id + 'input'}
               value={yearName}
               className={clsx(
-                { 'cursor-move': !mode || (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap)},
+                {
+                  'cursor-move':
+                    !mode ||
+                    (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap),
+                },
                 'flex-grow w-auto mt-auto font-semibold bg-transparent border-b border-transparent select-none text-md focus:border-gray-400 focus:outline-none',
               )}
               onChange={handleYearNameChange}
@@ -412,7 +419,11 @@ const YearComponent: FC<{
             <div className="flex-grow my-auto">
               <div
                 className={clsx(
-                  { 'cursor-move': !mode || (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap)},
+                  {
+                    'cursor-move':
+                      !mode ||
+                      (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap),
+                  },
                   'w-auto text-xl font-semibold bg-transparent border-b border-transparent select-none focus:border-gray-400 focus:outline-none',
                 )}
               >
@@ -490,7 +501,8 @@ const YearComponent: FC<{
               )}
               <div className="font-bold">{totalCredits} Credits</div>
             </div>
-            {(!mode || (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap)) && (
+            {(!mode ||
+              (mode !== ReviewMode.View && mode !== ReviewMode.RoadMap)) && (
               <DotsVerticalIcon
                 onClick={() => setDisplay(!display)}
                 className="cursor-pointer stroke-2 w-7"
--- lib/components/roadmap/search/SearchHeader.tsx
+++ lib/components/roadmap/search/SearchHeader.tsx
@@ -28,63 +28,62 @@ const SearchHeader: React.FC = () => {
 
   return (
     <>
-    <div className="sticky top-0 z-40 flex justify-between items-center py-1 px-4 h-1/6 bg-blue-header">
-      <div className="flex-grow">
-        <div className="inline-flex">
-          <img
-            className="w-12 h-12 mr-1 scale-x-[-1]"
-            src="/img/logo.png"
-            alt="logo"
-          />
+      <div className="sticky top-0 z-40 flex justify-between items-center py-1 px-4 h-1/6 bg-blue-header">
+        <div className="flex-grow">
+          <div className="inline-flex">
+            <img
+              className="w-12 h-12 mr-1 scale-x-[-1]"
+              src="/img/logo.png"
+              alt="logo"
+            />
 
-          <div
-            className="text-3xl cursor-pointer text-blue-footer self-center"
-            onClick={() => router.push('/')}
-          >
-            uCredit
+            <div
+              className="text-3xl cursor-pointer text-blue-footer self-center"
+              onClick={() => router.push('/')}
+            >
+              uCredit
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="relative sm:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+
+            {showMenu && (
+              <div className="absolute w-20 top-12 space-y-1 right-0 z-100 bg-white shadow-xl rounded-lg">
+                <Links />
+              </div>
+            )}
+          </div>
+
+          <div className="hidden sm:block">
+            <Links />
           </div>
         </div>
       </div>
-      <div>
-        <div className="relative sm:hidden">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-            onClick={() => setShowMenu(!showMenu)}
-          >
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-
-          {showMenu && (
-            <div className="absolute w-20 top-12 space-y-1 right-0 z-100 bg-white shadow-xl rounded-lg">
-              <Links />
-            </div>
-          )}
-        </div>
-
-        <div className="hidden sm:block">
-          <Links />
-        </div>
-      </div>
-      
-    </div>
-    <SearchBarArea />
+      <SearchBarArea />
     </>
   );
 };
