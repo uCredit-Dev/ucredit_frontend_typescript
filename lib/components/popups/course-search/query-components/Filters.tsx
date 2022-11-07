ee6afd2bb3a0855912bb2602b8c800ed6ed4168c
ml <mliu78@jh.edu>
2022-11-07 18:30:20 -0500

    feat: Updated footer ToS

--- lib/components/popups/course-search/query-components/Filters.tsx
+++ lib/components/popups/course-search/query-components/Filters.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Select from 'react-select';
 import ReactTooltip from 'react-tooltip';
