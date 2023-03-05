275211d2a3ab0b17718d45148aa60a9f5b4c3f54
Josephine Deng <jodeng256@gmail.com>
2023-03-05 15:37:50 -0500

    DEV-10 hopefully fix eslint errors

--- lib/components/dashboard/menus/comments/CommenterToggle.tsx
+++ lib/components/dashboard/menus/comments/CommenterToggle.tsx
@@ -43,8 +43,8 @@ const CommenterToggle: React.FC<Props> = ({ className = '' }) => {
       (newValue as any).comments = Array.from(filteredComments.values());
       filtered.set(key, newValue);
     }
-    for (let [key, value] of filtered.entries()) {
-      if (value.comments.length == 0) return;
+    for (let value of filtered.values()) {
+      if (value.comments.length === 0) return;
     }
     dispatch(updateFilteredThreads(filtered));
     // eslint-disable-next-line react-hooks/exhaustive-deps
