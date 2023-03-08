783b8a86c009264c83875d3c0dbb74a99d36c694
ml <mattliujhu@gmail.com>
2023-03-08 13:14:55 -0500

    fix: screen of course display is not size of screen anymore

--- lib/components/popups/CourseDisplayPopup.tsx
+++ lib/components/popups/CourseDisplayPopup.tsx
@@ -233,7 +233,7 @@ const CourseDisplayPopup: FC = () => {
       ></div>
 
       {/* Actual popup */}
-      <div className="h-screen fixed z-40 left-1/2 flex flex-col min-w-planAdd h-3/4 bg-primary rounded select-none transform -translate-x-1/2 translate-y-12">
+      <div className="fixed z-40 left-1/2 flex flex-col min-w-planAdd h-3/4 bg-primary rounded select-none transform -translate-x-1/2 translate-y-12">
         <div className="px-4 py-2 text-white text-coursecard font-semibold select-none">
           Inspecting{' '}
           {courseToShow === null ? 'Invalid course' : courseToShow.title}
