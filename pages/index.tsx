9a52fcf0e7124abc25705b7a0cd0b78b15140dd1
mpark63 <minspk13@gmail.com>
2023-03-25 14:40:27 -0400

    IN-49: add HTML tag verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,7 @@ const Home: React.FC = () => {
     <>
       <Head>
         <title>uCredit</title>
+        <meta name="google-site-verification" content="pvEeXuYCxF6NJiH_huGZMae_aKeTckqDrTB74w7FdF8" />
       </Head>
       <LandingPage />
     </>
