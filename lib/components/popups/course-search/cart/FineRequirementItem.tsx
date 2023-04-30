c3ac6e8ff8483489e40b33966131a548419d25dd
jchen324 <jchen324@jh.edu>
2023-04-30 15:47:31 -0400

    DEV-178 Add documentation for fine requirement components

--- lib/components/popups/course-search/cart/FineRequirementItem.tsx
+++ lib/components/popups/course-search/cart/FineRequirementItem.tsx
@@ -3,7 +3,13 @@ import parse from 'html-react-parser';
 import clsx from 'clsx';
 import { requirements } from '../../../dashboard/degree-info/distributionFunctions';
 
-// TODO : documentation goes here
+/**
+ * This component represents a single fine requirement item within the FineRequirementsList.
+ * @prop itemRequirement - An object containing information about the fine requirement.
+ * @prop onClick - A function to call when the fine requirement item is clicked, which will update the currently selected requirement.
+ * @prop selected - Optional boolean indicating whether the fine requirement is currently selected.
+ * @prop id - The ID of the fine requirement item.
+ */
 const FineRequirementListItem: FC<{
   itemRequirement: requirements;
   onClick: Function;
