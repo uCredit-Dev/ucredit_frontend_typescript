882e4473302b83659108ae35dd8726494dfe8802
Aprina Wang <awang100@jhu.edu>
2023-05-15 11:18:45 -0400

    DEV-71: run prettier

--- lib/components/popups/course-search/search-results/SisCourse.tsx
+++ lib/components/popups/course-search/search-results/SisCourse.tsx
@@ -405,7 +405,9 @@ const SisCourse: FC<{
 
   const getTermString = (year_id: string, semester: string | undefined) => {
     const year = currentPlan.years.find((y) => y._id === year_id);
-    const semesterFormatted = semester ? semester.charAt(0).toUpperCase() + semester.slice(1) : '';
+    const semesterFormatted = semester
+      ? semester.charAt(0).toUpperCase() + semester.slice(1)
+      : '';
     return (year ? year.name : '') + ' ' + semesterFormatted;
   };
 
