8fe7fdc56fbca24e7bca977f5329ad2a738083f9
mpark63 <minspk13@gmail.com>
2023-09-22 20:42:50 -0400

    Orange cringe, red pog - JO

--- lib/components/popups/course-search/search-results/CourseVersion.tsx
+++ lib/components/popups/course-search/search-results/CourseVersion.tsx
@@ -293,7 +293,7 @@ const CourseVersion: FC<{ setInspectedArea: (area: string) => void }> = ({
               <div className="absolute top-0 right-0">
                 <div
                   className="flex justify-center items-center w-7 h-7 text-sm font-semibold text-black transition duration-200 ease-in transform rounded-full bg-gray-200 hover:scale-110"
-                  data-tooltip-content={`Green: Satisfied all prerequisites. Orange: Not all prerequisites satisfied.`}
+                  data-tooltip-content={`Green: Satisfied all prerequisites. Red: Not all prerequisites satisfied.`}
                   data-tooltip-id="godtip"
                 >
                   ?
